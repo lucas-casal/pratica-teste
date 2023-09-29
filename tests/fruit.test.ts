2b2ca7994a2ac25add016d3b252a0cef9084c479
Lucas Casal <lucasocteixeira@gmail.com>
2023-09-29 18:07:36 -0300

    versaõ final

--- tests/fruit.test.ts
+++ tests/fruit.test.ts
@@ -10,13 +10,29 @@ describe("POST /fruits", () => {
     const result = await supertest(app).post('/fruits').send(fruit)
     const status = result.status;
     expect(status).toEqual(201)
+  })
+    it("given an already included value should return 409", async () => {
+      const fruit = {
+        name: 'banana',
+        price: '4000'
+      }
+      await supertest(app).post('/fruits').send(fruit)
+
+      const result = await supertest(app).post('/fruits').send(fruit)
+      const status = result.status;
+      expect(status).toEqual(409)
   });
 
-  it("given an invalid task it should return 422", () => {
-    // Aqui vai o código desse teste
+  it("given an invalid task it should return 422", async () => {
+    const fruit = {
+      name: 'banana',
+      price: 'ascas'
+    }
+    await supertest(app).post('/fruits').send(fruit)
+
+    const result = await supertest(app).post('/fruits').send(fruit)
+    const status = result.status;
+    expect(status).toEqual(422)
   });
 
-  it("given a task with duplicate title it should return 409", () => {
-    // Aqui vai o código desse teste
-  });
 });
